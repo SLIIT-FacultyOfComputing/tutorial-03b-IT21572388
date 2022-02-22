efb49f1b452ec842c3f4e22f3a1e89bc0e409b65
IT21572388 <99178857+IT21572388@users.noreply.github.com>
2022-02-22 20:14:49 +0000

    Tutorial 3 completed

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,14 @@
-#include <iostream>
-using namespace std;
-int main() {
- float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
- char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
- for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
- }
+#include<iostream>
+#include<iomanip>
+using namespace std ;
+int main(){
+	cout << "123456789012345678901234567890" << endl;
+	float marks[] = { 78.4 , 90.3 , 45.9 , 72.2 , 54.4};
+	char names[][20] = {"Ajith" , "Wimal" ,"Kanthi" , "Suranji" , "Kushitha"};
+	cout << setw(5) << "No" << setw(15) <<"Name" << setw(10) << "Marks" << endl;
+	for(int r = 0 ; r < 5 ; r++){
+		cout << setw(5) << r + 1 
+	    << setw(15) << names[r] 
+	    << setiosflags (ios::fixed)<< setw(10)<< setprecision (2) << marks[r] << endl;
+	}
 }
